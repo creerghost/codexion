c0d2946418ac3c874b3d1dae8c3b6c9b6dc46a27
creerghost <nikolaevvova20vova03@gmail.com>
2026-09-13 23:18:25 +0200

    fixed logger

--- src/coder/coder_data.c
+++ src/coder/coder_data.c
@@ -6,7 +6,7 @@
 /*   By: vlnikola <vlnikola@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 23:30:00 by vlnikola          #+#    #+#             */
-/*   Updated: 2026/09/13 22:41:17 by vlnikola         ###   ########.fr       */
+/*   Updated: 2026/09/13 23:14:22 by vlnikola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,16 @@ int	coder_compile_count(t_coder *coder)
 	compiles_done = coder->compiles_done;
 	pthread_mutex_unlock(&coder->data_mutex);
 	return (compiles_done);
+}
+
+long long	coder_last_compile(t_coder *coder)
+{
+	long long	last_compile;
+
+	if (coder == NULL)
+		return (-1);
+	pthread_mutex_lock(&coder->data_mutex);
+	last_compile = coder->last_compile_start;
+	pthread_mutex_unlock(&coder->data_mutex);
+	return (last_compile);
 }
--- src/utils/logger.c
+++ src/utils/logger.c
@@ -6,23 +6,28 @@
 /*   By: vlnikola <vlnikola@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 23:30:00 by vlnikola          #+#    #+#             */
-/*   Updated: 2026/09/13 22:49:10 by vlnikola         ###   ########.fr       */
+/*   Updated: 2026/09/13 23:17:48 by vlnikola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "modules/logger_api.h"
+#include "modules/time_api.h"
+#include "modules/context_api.h"
 #include <stdio.h>
 
 void	log_state(t_context *context, int coder_id, const char *state)
 {
-	pthread_mutex_lock(&context->state_mutex);
-	printf("%d %lld %s\n", coder_id, context->start_time, state);
-	pthread_mutex_unlock(&context->state_mutex);
+	pthread_mutex_lock(&context->log_mutex);
+	if (context_is_running(context))
+		printf("%lld %i %s\n",
+			simulation_time_ms(context), coder_id, state);
+	pthread_mutex_unlock(&context->log_mutex);
 }
 
 void	log_burnout(t_context *context, int coder_id)
 {
 	pthread_mutex_lock(&context->log_mutex);
-	printf("%i burned out\n", coder_id);
-	pthread_mutex_lock(&context->log_mutex);
+	printf("%lld %d burned out\n",
+		simulation_time_ms(context), coder_id);
+	pthread_mutex_unlock(&context->log_mutex);
 }
