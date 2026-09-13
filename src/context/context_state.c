571050aec7910731dc95bf0db8d21770ddcec363
Vladimir Nikolaev <vlnikola@c1r3s7.42prague.com>
2026-09-13 17:48:55 +0200

    add: context fully done

--- src/context/context_state.c
+++ src/context/context_state.c
@@ -3,11 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   context_state.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlnikola <vlnikola@student.42prague.com    +#+  +:+       +#+        */
+/*   By: vlnikola <vlnikola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 23:30:00 by vlnikola          #+#    #+#             */
-/*   Updated: 2026/09/12 23:30:00 by vlnikola         ###   ########.fr       */
+/*   Updated: 2026/09/13 17:46:59 by vlnikola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "modules/context_api.h"
+
+bool	context_is_running(t_context *context)
+{
+	bool	running;
+
+	if (context == NULL)
+		return (false);
+	pthread_mutex_lock(&context->state_mutex);
+	running = (context->is_running == 0);
+	pthread_mutex_unlock(&context->state_mutex);
+	return (running);	
+}
